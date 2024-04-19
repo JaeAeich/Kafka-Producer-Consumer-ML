be4f9c94630622980c62515f3030cf5a3df1db54
coder-Ace77 <adil7k777@gmail.com>
2024-04-19 20:00:23 +0530

    Model almost done

--- consumer/app.py
+++ consumer/app.py
@@ -1,64 +1,77 @@
 import os
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 from kafka import KafkaConsumer
 import asyncio
 import json
+from support_files.fifo import FifoBuffer
+import pandas as pd
+from support_files.model import Stock_Predictor
+import datetime
 
-# Load environment variables
-load_dotenv()
+# load_dotenv()
 
-def modelTrain(model, data):
-    pass
-
-def modelPredict(model, data):
-    return data  # {name, close, time, open, high, low}
+MAX_MEMORY = 1000  # Maximum number of data points to store in memory
 
 # configurations
-bootstrap_servers = os.environ.get("KAFKA_SERVER")
-topic = os.environ.get("KAFKA_TOPIC")
-delay = int(os.environ.get("CONSUMER_DELAY"))
+# bootstrap_servers = os.environ.get("KAFKA_SERVER")
+# topic = os.environ.get("KAFKA_TOPIC")
+# delay = int(os.environ.get("CONSUMER_DELAY"))
+
+bootstrap_servers = "localhost:29092"
+topic = "test_topic"
+delay = 5
 
 print("#######################################")
 print("bootstrap_servers: ", bootstrap_servers)
 print("topic: ", topic)
-# print("consumer delay: ", delay)
 print("#######################################")
 
+buffer_backup = "buffer_backup.json"
+
 # Create Kafka consumer
-consumer = KafkaConsumer(topic,
-                         group_id='my-group',
-                         bootstrap_servers=bootstrap_servers,
-                         auto_offset_reset='earliest',
-                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+consumer = KafkaConsumer(
+    topic,
+    group_id="my-group",
+    bootstrap_servers=bootstrap_servers,
+    auto_offset_reset="earliest",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+)
 
 consumer.subscribe(topic)
+buffer = {}
 
-# Function to receive messages from Kafka
-async def get_message_from_kafka():
-    for message in consumer:
-        yield message.value
 
-# Function to receive messages from Kafka for 5 seconds
-async def consume_messages():
-    buffer = {}
-    start_time = asyncio.get_event_loop().time()  # Get the current event loop's time
-    
-    # Consume messages for 5 seconds
-    async for message in get_message_from_kafka():
-        # buffer.append(message)
-        message = json.loads(message)
-        if(buffer.get(message['name']) is None):
-            buffer[message['name']] = []
-        buffer[message['name']].append(message)
-        if asyncio.get_event_loop().time() - start_time >= delay:
-            break
-    print(buffer.keys())
-    return buffer
+async def consume_messages(model):
+    l = []
+    for i in buffer.values():
+        l.append(i.getall())
+    if l is None or len(l) == 0:
+        return
+    dfs = pd.concat(l, ignore_index=True)
+    model.train_model(dfs)
 
+
+TIME_INTERVAL_OF_TRAIN = 100
 if __name__ == "__main__":
-    while True:
-        # Consuming messages
-        data_from_kafka = asyncio.run(consume_messages())
-        model = "Model"  # TODO: load model when ready
-        modelTrain(model, data_from_kafka)
-        # print(modelPredict(model, data_from_kafka))  # TODO: send data to UI
+    i = 0
+    model = Stock_Predictor("model.keras", 10)
+    for message in consumer:
+        i += 1
+        i = i % TIME_INTERVAL_OF_TRAIN
+        print(i)
+        if i == 0:
+            data_from_kafka = asyncio.run(consume_messages(model))
+        message = json.loads(message.value)
+        if buffer.get(message["name"]) is None:
+            buffer[message["name"]] = FifoBuffer(MAX_MEMORY)
+        buffer[message["name"]].insert(message)
+        if buffer[message["name"]].getlatest().shape[0] >= 10:
+            current_time = datetime.datetime.now()
+            formatted_time = current_time.strftime("%H:%M:%S")
+            print("Current time:", formatted_time)
+            print(
+                message["name"],
+                message["close"],
+                model.predict(buffer[message["name"]].getlatest())["close"],
+            )
